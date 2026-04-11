63e783971892d602af4284fa96e1ed193d23d00a
Joseph Shapiro <joeyashapiro@gmail.com>
2026-04-11 15:20:18 -0500

    re-added sign/seal flags

--- negotiate_message.go
+++ negotiate_message.go
@@ -28,7 +28,9 @@ var defaultFlags = negotiateFlagNTLMSSPNEGOTIATETARGETINFO |
 	negotiateFlagNTLMSSPNEGOTIATEUNICODE |
 	negotiateFlagNTLMSSPNEGOTIATEEXTENDEDSESSIONSECURITY |
 	negotiateFlagNTLMSSPNEGOTIATENTLM |
-	negotiateFlagNTLMSSPNEGOTIATEKEYEXCH
+	negotiateFlagNTLMSSPNEGOTIATEKEYEXCH |
+	negotiateFlagNTLMSSPNEGOTIATESIGN |
+	negotiateFlagNTLMSSPNEGOTIATESEAL
 
 // NewNegotiateMessage creates a new NEGOTIATE message with the flags that this package supports.
 // Note that domain and workstation refer to the client machine, not the user that is authenticating.
